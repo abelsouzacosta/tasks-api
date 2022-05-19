3abb1f0e467bf04f01dcacda7c72bf9a025ada1f
junior <abelsouzacosta@gmail.com>
2022-05-19 01:01:44 -0300

    [Refactor](tasks): create
    removing comments

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,9 +11,6 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { CreateTaskDto } from './dtos/create-task.dto';
-import { GetTasksFilterDto } from './dtos/get-task-filter.dto';
-import { UdpateTaskDto } from './dtos/update-task.dto';
-import { Task } from './task.entity';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
@@ -25,37 +22,4 @@ export class TasksController {
   async create(@Body() data: CreateTaskDto): Promise<void> {
     return this.tasksService.create(data);
   }
-
-  // @Get()
-  // async list(@Query() filterDto: GetTasksFilterDto): Promise<Task[]> {
-  //   if (Object.keys(filterDto).length) {
-  //     return this.tasksService.getTasksWithFilters(filterDto);
-  //   } else {
-  //     return this.tasksService.getTasks();
-  //   }
-  // }
-
-  // @Get('/:id')
-  // async getTaskById(@Param('id') id: string): Promise<Task> {
-  //   return this.tasksService.getTaskById(id);
-  // }
-
-  // @Post()
-  // @UsePipes(new ValidationPipe())
-  // async create(@Body() body: CreateTaskDto): Promise<void> {
-  //   return this.tasksService.createTask(body);
-  // }
-
-  // @Patch('/:id')
-  // async update(
-  //   @Param('id') id: string,
-  //   @Body() body: UdpateTaskDto,
-  // ): Promise<void> {
-  //   return this.tasksService.updateTask(id, body);
-  // }
-
-  // @Delete('/:id')
-  // async delete(@Param('id') id: string): Promise<void> {
-  //   return this.tasksService.deleteTask(id);
-  // }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { TaskStatus } from './task-status.enum.';
-import { Task } from './task.entity';
 import { CreateTaskDto } from './dtos/create-task.dto';
-import { UdpateTaskDto } from './dtos/update-task.dto';
-import { GetTasksFilterDto } from './dtos/get-task-filter.dto';
 import { TasksRepository } from './tasks.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 
@@ -22,61 +18,4 @@ export class TasksService {
 
     await this.tasksRepository.save(task);
   }
-
-  // async getTasks(): Promise<Task[]> {
-  //   return this.tasks;
-  // }
-
-  // async getTasksWithFilters(filterDto: GetTasksFilterDto): Promise<Task[]> {
-  //   const { search, status } = filterDto;
-  //   let tasks = await this.getTasks();
-
-  //   if (status) {
-  //     tasks = tasks.filter((task) => task.status === status);
-  //   }
-
-  //   if (search) {
-  //     tasks = tasks.filter((task) => {
-  //       if (task.title.includes(search) || task.description.includes(search)) {
-  //         return true;
-  //       }
-
-  //       return false;
-  //     });
-  //   }
-
-  //   return tasks;
-  // }
-
-  // async getTaskById(id: string): Promise<Task> {
-  //   const task = this.tasks.find((task) => task.id === id);
-
-  //   return task;
-  // }
-
-  // async createTask(taskBody: CreateTaskDto): Promise<void> {
-  //   const task: Task = {
-  //     id: uuid(),
-  //     title: taskBody.title,
-  //     description: taskBody.description,
-  //     status: TaskStatus.OPEN,
-  //   };
-
-  //   this.tasks.push(task);
-  // }
-
-  // async updateTask(id: string, body: UdpateTaskDto): Promise<void> {
-  //   const task = this.tasks.find((task) => task.id === id);
-
-  //   task.title = body.title || task.title;
-  //   task.description = body.description || task.description;
-  // }
-
-  // async deleteTask(id: string): Promise<void> {
-  //   const task = this.tasks.find((task) => task.id === id);
-
-  //   const indexOfTask = this.tasks.indexOf(task);
-
-  //   this.tasks.splice(indexOfTask, 1);
-  // }
 }
