bcab3fa491ba1ad15c2afe0c2d86c76ed4058c1b
junior <abelsouzacosta@gmail.com>
2022-05-25 01:55:17 -0300

    [Feat](pipes): using pipes
    Using validation pipes and a pipe to veriufy if some instance exists

--- src/tasks/dtos/update-task.dto.ts
+++ src/tasks/dtos/update-task.dto.ts
@@ -1,4 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional, IsString } from 'class-validator';
 import { CreateTaskDto } from './create-task.dto';
 
-export class UdpateTaskDto extends PartialType(CreateTaskDto) {}
+export class UdpateTaskDto extends PartialType(CreateTaskDto) {
+  @IsOptional()
+  @IsString()
+  title?: string;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
