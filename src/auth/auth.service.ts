8e5dfb14bffe22f1d6d805b5afd9dd22859fc138
junior <abelsouzacosta@gmail.com>
2022-05-27 00:55:59 -0300

    [Refactor](users): remove non used methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,6 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dtos/create-user.dto';
-import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
 
 @Injectable()
@@ -11,23 +10,7 @@ export class AuthService {
     private userRepository: UsersRepository,
   ) {}
 
-  private async getUserByUsername(username: string): Promise<User> {
-    const user = await this.userRepository.findOne({
-      where: {
-        username,
-      },
-    });
-
-    return user;
-  }
-
   async createUser(body: CreateUserDto): Promise<void> {
-    const userAlreadyExists = await this.getUserByUsername(body.username);
-
-    if (userAlreadyExists) {
-      throw new HttpException(`Duplicated username`, HttpStatus.CONFLICT);
-    }
-
     await this.userRepository.createUser(body);
   }
 }
