bd10cae4119011b0ce99dbeff339e7972601ad9b
junior <abelsouzacosta@gmail.com>
2022-06-04 04:40:13 -0300

    [Feat]: adding relationship bewtween tasks and users

--- src/tasks/task.entity.ts
+++ src/tasks/task.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/auth/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { TaskStatus } from './enums/task-status.enum.';
 
 @Entity('tasks')
@@ -14,4 +15,7 @@ export class Task {
 
   @Column({ default: TaskStatus.OPEN })
   status: TaskStatus;
+
+  @ManyToOne(() => User, (user) => user.tasks, { eager: false })
+  user: User;
 }
